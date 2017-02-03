65e34bfa0ef3f36fb84a2487cb096dd6f517d602
Damien RAYMOND <dam.ray49@gmail.com>
2017-02-03 15:49:05 +0100

    Add Bookings (link between WS and message queue) and update WebServices.Controllers

---
+++ Bookings/BookingsManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookings
+{
+    public class BookingsManager
+    {
+
+        private const string QUEUE_NAME = "bankings";
+
+        public BookingsManager()
+        {
+
+        }
+
+        public void Reserve(BookingsData data)
+        {
+            Console.WriteLine("Reserving : " + data);
+            MessageAppender appender = new MessageAppender(QUEUE_NAME);
+            appender.append(data);
+        }
+
+    }
+
+
+    public class MessageAppender
+    {
+
+        private string queueName;
+
+        public MessageAppender(string queue)
+        {
+            queueName = queue;
+        }
+
+        public void append(BookingsData bookingData)
+        {
+            MessageQueue MyMQ = new MessageQueue(@".\private$\" + queueName);
+            MyMQ.Send(bookingData, "Transfert");
+            MyMQ.Close();
+        }
+    }
+
+
+    public class BookingsData
+    {
+        // flight
+        public string departure_city;
+        public DateTime departure_date;
+        public string arrival_city;
+        public string company;
+
+        // hotel
+        public string city;
+        public string name;
+        public string rating;
+
+        // user
+        public string rib;
+        public string email;
+        public string first_name;
+        public string last_name;
+    }
+
+}
--- WebServices/Controllers/TripsController.cs
+++ WebServices/Controllers/TripsController.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using Newtonsoft.Json.Linq;
+using System.Data;
+using Newtonsoft.Json;
+using Bookings;
+using System.ComponentModel;
 
 namespace WebServices.Controllers
 {
@@ -17,10 +21,46 @@ namespace WebServices.Controllers
         }
 
         // POST api/trips
-        public void Post([FromBody]string value)
+        public void Post([FromBody]dynamic value)
         {
-            JObject json = JObject.Parse(value);
-            Console.WriteLine("coucou");
+            Console.WriteLine(value);
+            BookingsData data = new BookingsData();
+
+            // flight
+            data.departure_city = get(data, "departure_city");
+            data.departure_date = value.GetType().GetProperty("departure_date").GetValue(value, null);
+            data.arrival_city = get(data, "arrival_city");
+            data.company = get(data, "company");
+
+            // hotel
+            data.city = get(data, "city");
+            data.name = get(data, "name");
+            data.rating = get(data, "rating");
+
+            // user
+            data.rib = get(data, "rib");
+            data.email = get(data, "email");
+            data.first_name = get(data, "first_name");
+            data.last_name = get(data, "last_name");
+
+
+            BookingsManager b = new BookingsManager();
+            b.Reserve(data);
+
+
+            //DataTable dt = ConvertToDataTable(value);
+            //Console.WriteLine(dt);
+            /* JObject json = JObject.Parse(value);
+             Console.WriteLine("receive post");
+             DataTable dt = JsonConvert.DeserializeObject<DataTable>(json.ToString());
+             BookingsManager b = new BookingsManager();
+             b.Reserve(dt);*/
         }
+
+        private string get(dynamic value, string identifier)
+        {
+            return value.GetType().GetProperty(identifier).GetValue(value, null);
+        }
+        
     }
 }
