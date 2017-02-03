c5ccbfae4246a387a998c35947b75b48c8765643
Anael CHARDAN <anael.chardan@gmail.com>
2017-02-03 10:06:53 +0100

    Add an API using sharp

---
+++ WebServices/Controllers/FlightsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebServices.Controllers
+{
+    public class FlightsController : ApiController
+    {
+        // GET api/flights
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "flight1", "flight2" };
+        }
+    }
+}
