16ce31dfa67465b0e502626f04735240ca939be3
Clément Garbay <clementgarbay@gmail.com>
2017-02-04 18:40:13 +0100

    Load dependencies fully with webpack

---
+++ UI/src/dependencies.js
@@ -0,0 +1,5 @@
+import 'jquery'
+import 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'ui-select/dist/select.css'
--- UI/webpack.config.js
+++ UI/webpack.config.js
@@ -1,13 +1,41 @@
+const webpack = require('webpack')
+
 module.exports = {
   context: __dirname,
 
   entry: {
-    app: './src/app.js'
+    app: ['./src/app.js', './src/dependencies.js']
   },
 
   output: {
     path: './public',
     filename: 'app.bundle.js'
-  }
+  },
 
+  devtool: 'source-map',
+
+  module: {
+    loaders: [
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery',
+      $: 'jquery',
+      jquery: 'jquery'
+    })
+  ]
 }
