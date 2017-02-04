7d401857b8e118d0201c64513544d43dc0a1b578
Clément Garbay <clementgarbay@gmail.com>
2017-02-04 18:39:32 +0100

    Connect UI to API format

---
+++ UI/src/models/state.js
@@ -0,0 +1,17 @@
+import { Record } from 'immutable'
+
+export default Record({
+  departureCity: null,
+  arrivalCity: null,
+  departureDate: null,
+
+  flight: null,
+  hotel: null,
+
+  user: {
+    firstName: null,
+    lastName: null,
+    email: null,
+    rib: null
+  }
+})
